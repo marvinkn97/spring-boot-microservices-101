563dd76892a04be92dd9033795d5fe235d7b2d01
marvinkn97 <marvin.nyingi97@gmail.com>
2025-02-01 21:54:59 +0300

    intro to testcontainers

--- embarkX - Faisal Memon/job-listing/src/test/java/dev/marvin/TestContainersTest.java
+++ embarkX - Faisal Memon/job-listing/src/test/java/dev/marvin/TestContainersTest.java
@@ -1,9 +1,16 @@
 package dev.marvin;
 
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class TestContainersTest {
-    private final static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
+    public final static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
+
+    private static void registerDynamicDBProps(DynamicPropertyRegistry dynamicPropertyRegistry){
+        dynamicPropertyRegistry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 }
